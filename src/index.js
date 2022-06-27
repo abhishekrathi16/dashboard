0689625349e45b38be5152ae0bfbb0648d87eded
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-06-27 18:48:00 +0530

    create dashboard

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { ContextProvider } from "./contexts/ContextProvider";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+    <App />
+  </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
